7f15f0200820bc91d11013a1cef4424c094c43a6
Wonderboy Mthiyane <techmanifestsa@gmail.com>
2024-02-22 13:46:34 +0200

    Update: Server Methods and Routes, Handle req/res

--- main.go
+++ main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprint(w, "ParseForm() err: %v", err)
+		return
+	}
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "POST" {
+		http.Error(w, "method not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	surname := r.FormValue("surname")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	fmt.Fprintf(w, "Name =%s\n", name)
+	fmt.Fprintf(w, "Surname =%s\n", surname)
+	fmt.Fprintf(w, "Email =%s\n", email)
+	fmt.Fprintf(w, "Password =%s\n", password)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "method not suppoted", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello")
+}
+
+func main() {
+	fileSever := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fileSever)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Print("Sever Is Running !!!")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
